86f77b362907675b32996df42b9f21b7938aa081
ysrkw <15919660+ysrkw@users.noreply.github.com>
2024-06-12 04:12:24 +0900

    fix tests

--- modules/shared/entity.test.ts
+++ modules/shared/entity.test.ts
@@ -1,4 +1,4 @@
-import { assertInstanceOf } from "@std/assert";
+import { assertInstanceOf, assertStrictEquals } from "@std/assert";
 import { Entity } from "./entity.ts";
 
 interface UserProps {
@@ -11,4 +11,24 @@ Deno.test(function createEntity() {
   const instance = new User({ name: "john doe" });
 
   assertInstanceOf(instance, User);
+});
+
+Deno.test(function holdArgumentProps() {
+  const instance = new User({ name: "john doe" });
+
+  assertStrictEquals(instance.props.name, "john doe");
+});
+
+Deno.test(function sameIdentifiesIsEqual() {
+  const alice = new User({ name: "alice" });
+  const bob = new User({ name: "bob" }, alice.id);
+
+  assertStrictEquals(alice.equals(bob), true);
+});
+
+Deno.test(function differentIdentifiesIsNotEqual() {
+  const alice = new User({ name: "alice" });
+  const bob = new User({ name: "bob" });
+
+  assertStrictEquals(alice.equals(bob), false);
 });
