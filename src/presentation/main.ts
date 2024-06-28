72ab804fb3431ebbd4a96f9455ac45f443159e93
ysrkw <15919660+ysrkw@users.noreply.github.com>
2024-06-29 03:19:13 +0900

    add middleware

--- src/presentation/main.ts
+++ src/presentation/main.ts
@@ -1,6 +1,11 @@
 import { Hono } from "@hono/hono";
+import { cors } from "@hono/hono/cors";
+import { logger } from "@hono/hono/logger";
 
 const app = new Hono();
+
+app.use(cors());
+app.use(logger());
 
 app.get("/", (c) => c.text("Hono!"));
 
