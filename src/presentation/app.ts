736ba0253808ff342a97a6c59a0cf4817b101dfa
ysrkw <15919660+ysrkw@users.noreply.github.com>
2024-07-02 03:44:31 +0900

    add hono kv

--- src/presentation/app.ts
+++ src/presentation/app.ts
@@ -5,11 +5,21 @@ import { secureHeaders } from "@hono/hono/secure-headers";
 import { index } from "./routes/index.tsx";
 import { login } from "./routes/login.tsx";
 
-export const app = new Hono();
+export const app = new Hono<{ Variables: { kv: Deno.Kv } }>();
 
 app.use(cors());
 app.use(logger());
 app.use(secureHeaders());
+
+app.use(async (ctx, next) => {
+  const kv = await Deno.openKv(":memory:");
+
+  ctx.set("kv", kv);
+
+  await next();
+
+  kv.close();
+});
 
 app.route("/", index);
 app.route("/login", login);
